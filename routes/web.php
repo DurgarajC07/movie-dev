edcb1b06d191f981ed4bd66f1f90ac622654834c
DurgarajC07 <60312747+DurgarajC07@users.noreply.github.com>
2024-04-28 15:03:19 +0530

    first
     commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Front\FrontendController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,4 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontendController::class, 'index']);
